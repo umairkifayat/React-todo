e43c21f7ac5d0435522281a7cae7117d8c749b49
umair kifayat <umairkifayat3@gmail.com>
2024-01-23 15:27:46 -0800

    'updated'

--- todo/src/screens/home/Home.jsx
+++ todo/src/screens/home/Home.jsx
@@ -4,8 +4,8 @@ const Home = () => {
   return (
     <>
   <form className='d-flex justify-content-center align-item-center h-80'>
-  <TextField id="outlined-basic" label="Add Todo" variant="outlined" />
-  <Button variant="contained" type="submit">Add List</Button>
+  <TextField id="outlined-basic" label="Add Todo"   variant="outlined" />
+  <Button variant="contained" type="submit">  Add List</Button>
   </form>
     </>
   )
